4eb1de7f8e9c66aa51c7bc5d7b76bcd44e5c60af
Edward Spencer <epfspencer@yahoo.com>
2020-05-27 20:04:17 +0100

    Improve prayer flicking
     - The ticks are constant within the game, so now
       the script keeps track of when the tick was started
       amd will start the next time it is turned on at the right
       time, instead of the user having judge when to turn it on

--- mousekeys/pray_one_tick.py
+++ mousekeys/pray_one_tick.py
@@ -1,5 +1,6 @@
 
 import time
+import keyboard
 
 from ..utils import utils
 
@@ -13,60 +14,66 @@ class PrayOneTick:
     SHORT_PAUSE = 0.2
     CYCLE = LONG_PAUSE + SHORT_PAUSE
 
+    ADJUST = 0.01
+
     def __init__(self):
         self.qp = utils.get_aoi()['0']
 
         self.on_at = None
         self.pray_on = False
         self.off_at = None
+        self.state = None
 
         print('loaded')
 
     def run(self):
 
-        clicked_at = None
-        started_at = None
+        started_at = time.time()
         flicks = 0
 
         while True:
 
-            if utils.on_off_state():
+            self.state = utils.on_off_state()
+            pause = (time.time() - (started_at + (flicks * self.CYCLE)))
 
-                if not started_at:
-                    started_at = time.time()
-                    flicks = 0
+            if keyboard.is_pressed('+'):
+                started_at += self.ADJUST
+                print(f'time moved forward {self.ADJUST}')
+                time.sleep(0.05)
+            elif keyboard.is_pressed('-'):
+                started_at -= self.ADJUST
+                print(f'time moved back {self.ADJUST}')
+                time.sleep(0.05)
 
+            if self.state:
+                # make sure mouse is inside quick prayer bbox
                 if not utils.mouse_inside(**self.qp):
                     utils.move_to_aoi(**self.qp)
-
-                pause = (time.time() - (started_at + (flicks * self.CYCLE)))
-
-                if self.pray_on:
-                    if pause > self.LONG_PAUSE:
-                        clicked_at = time.time()
-                        utils.wait_and_click(start=0.04, stop=0.09)
-                        self.pray_on = False
-                        flicks += 1
-                        print(f'flick {flicks} off at {clicked_at} - paused ({round(pause, 4)})')
-                else:
-                    if pause > 0:
-                        clicked_at = time.time()
-                        utils.wait_and_click(start=0.04, stop=0.09)
-                        self.pray_on = True
-                        print(f'flick {flicks} on at {clicked_at} - paused ({round(pause, 4)})')
-
             else:
-
-                # make sure prayer is off
+                # switch prayer off if it's not already
                 if self.pray_on:
                     if not utils.mouse_inside(**self.qp):
                         utils.move_to_aoi(**self.qp)
                     utils.wait_and_click(start=0.04, stop=0.09)
+                    self.pray_on = False
 
-                # reset other variables
-                self.pray_on = False
-                started_at = None
-                flicks = 0
+            if self.pray_on:
+                if pause > self.LONG_PAUSE:
+                    clicked_at = time.time()
 
-                # wait for something to happen
-                time.sleep(0.01)
+                    # if we're on then actually do it
+                    if self.state:
+                        utils.wait_and_click(start=0.04, stop=0.09)
+                        self.pray_on = False
+                        # print(f'flick {flicks} off at {clicked_at} - paused ({round(pause, 4)})')
+            else:
+                if pause > 0:
+                    clicked_at = time.time()
+                    flicks += 1
+
+                    # if we're on the actually do it
+                    if self.state:
+                        utils.wait_and_click(start=0.04, stop=0.09)
+                        self.pray_on = True
+
+                        # print(f'flick {flicks} on at {clicked_at} - paused ({round(pause, 4)})')
